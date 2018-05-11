a46ba035f62b90a799a9e9ba8a3da9e351de9f02
GustavoMalta <gustavo.a.malta@gmail.com>
2018-05-11 18:15:34 -0300

    Auth0 Meu

--- src/services/auth.config.ts
+++ src/services/auth.config.ts
@@ -2,9 +2,9 @@
 
 export const AUTH_CONFIG = {
   // Needed for Auth0 (capitalization: ID):
-  clientID: 'NBYBcD1zaOZFEFIXDoKXLahtB2xiQdU7',
+  clientID: 'S5cDB0BNPMV4BgDMvJxknExy5wbQfbj7',
   // Needed for Auth0Cordova (capitalization: Id):
-  clientId: 'NBYBcD1zaOZFEFIXDoKXLahtB2xiQdU7',
-  domain: 'fgcintra.auth0.com', // e.g., you.auth0.com
+  clientId: 'S5cDB0BNPMV4BgDMvJxknExy5wbQfbj7',
+  domain: 'gustavomalta.auth0.com', // e.g., you.auth0.com
   packageIdentifier: 'com.auth0.ionic' // config.xml widget ID
 };
